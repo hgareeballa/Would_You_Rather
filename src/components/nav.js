c6a72a7b0e83553843279cfa2a81ce56e52c1455
Hassan <sudane@gmail.com>
2020-03-29 13:52:08 +0000

    Update

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,41 +1,46 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink as Link } from 'react-router-dom';
 import '../navcss.css'
 
 class nav extends Component {
-    render() {
-        const {activeUser} = this.props
-        return (
+  render() {
+    const { activeUser } = this.props
+    return (
 
-          <div align="center">
-            <h2> <span className="badge badge-primary badge-pill align-items-center">Would You Rather v1.0 ?</span> </h2>  
-            {activeUser === null ? (
-            <h3> <span className="badge-pill btn btn-outline-primary">
-            <NavLink to="/login">Login</NavLink>
-            </span> </h3>                              
-              ) : (
-            <h3> <span className="badge-pill btn btn-outline-primary">
-            <NavLink to="/logout">Logout</NavLink></span> 
-            </h3>  
-            
-                )}
-            <h4>{activeUser === null ? "" : activeUser.name} {activeUser === null ? "" : <img src={activeUser.avatarURL} className="img-circle" alt="Cinque Terre" width="20" height="20" />} </h4>            
-            <ul className="nav" align="center">
-              <li><NavLink to="/questions"  activeStyle={{fontWeight: 'bold'}}> Questions</NavLink></li>
-              <li><NavLink to="/add"> Add Questions</NavLink></li>     
-              <li><NavLink to="/leaderboard"> Leadr Board</NavLink></li>                                                                   
-            </ul>
-          </div>
-        );
-    }
+      <div align="center">
+        <h2> <span className="badge badge-primary badge-pill align-items-center">Would You Rather v1.0 ?</span> </h2>
+
+        {activeUser === null ? <LoginOut value="login" /> : <LoginOut value="logout" />}
+
+        <h4>{activeUser === null ? "" : activeUser.name} {activeUser === null ? "" : <img src={activeUser.avatarURL} className="img-circle" alt="Cinque Terre" width="20" height="20" />} </h4>
+        <ul className="nav" align="center">
+          <li><Link to="/questions" activeStyle={{ fontWeight: 'bold' }}> Questions</Link></li>
+          <li><Link to="/add"> Add Questions</Link></li>
+          <li><Link to="/leaderboard"> Leadr Board</Link></li>
+        </ul>
+      </div >
+    );
+  }
 }//
 
-function mapStateToProps({activeUser}) {
-    return {
-        activeUser:activeUser===undefined?null:activeUser
-    };
+function mapStateToProps({ activeUser }) {
+  return {
+    activeUser: activeUser === undefined ? null : activeUser
+  };
 }
 export default connect(mapStateToProps)(nav);
+
+function LoginOut(props) {
+  const { value } = props;
+  return (
+    <div>
+      <h3>
+        <Link to={{ pathname: value }}><span className="badge-pill btn btn-outline-primary">{value}</span></Link>
+      </h3>
+    </div >
+  )
+
+}
 
 
